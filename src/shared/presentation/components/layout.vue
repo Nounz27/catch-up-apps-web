9e9238c9bab7408d011c8551a33642c526b25419
Nounz27 <mathiasenrique.sanchez@gmail.com>
2026-04-24 18:13:14 -0500

    feat(layout): add layout content.

---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {ref} from "vue";
+import {Button as PvButton, Menubar as PvMenubar} from "primevue";
+import LanguageSwitcher from "./language-switcher.vue";
+import FooterContent from "./footer-content.vue";
+
+const drawerVisible = ref(false);
+const toggleDrawer = () => {
+  draweVisible.value = !drawerVisible.value;
+};
+</script>
+
+<template>
+<div>
+  <div>
+    <pv-menubar>
+      <template #start>
+        <pv-button icon="pi pi-bars" @click= "toggleDrawer" label="CatchUp" text />
+        <!-- Add Source List here -->
+
+      </template>
+      <template #end>
+        <language-switcher/>
+      </template>
+    </pv-menubar>
+  </div>
+</div>
+<div>
+  <!-- Add Article List here -->
+  <!-- Unavailable Content Component here -->
+</div>
+<footer-content/>
+</template>
+
+<style scoped>
+
+</style>
